54c854fb55233daafba59729fa39159d8e552e4a
NouraNou <oneway.33@hotmail.com>
2023-07-08 13:46:38 +0300

    logout(done) profile(updated)

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import background3 from "../media/background2.jpg";
+import UserRecipeCard from "../components/UserRecipeCard";
 const Profile = () => {
   //   const queryClient = useQueryClient();
   //   const {
@@ -13,6 +14,16 @@ const Profile = () => {
   //   });
   return (
     <div className="h-full flex justify-content relative flex-col">
+      <h1>{User.name}</h1>
+
+      <h3>My Recipes</h3>
+
+      {/* <button
+        onclick={addApi}
+        className=" "
+      >
+        Add New Recipe
+      </button> */}
       <div
         className="bg-cover rounded-t-3xl bg-center w-full h-80  bg-no-repeat"
         style={{
@@ -24,6 +35,14 @@ const Profile = () => {
         {/* <img src={pet.image} className="w-full h-full object-cover" /> */}
       </div>
       <div className="flex  justify-start h-screen w-96 bg-gray-400">v</div>
+
+      <UserRecipeCard />
+
+      <Link to={`/SignUp`}>
+        <button className=" border border-black px-5 py-1 rounded-md hover:bg-[black] hover:text-white">
+          Create a new account
+        </button>
+      </Link>
     </div>
   );
 };
