7fc09d54fe945f8d7ba87e7e7473d7df5f49a5e6
MARIAM ALRASDAN <mariam.alrashdan97@gmail.com>
2023-07-10 22:25:03 +0300

    try

--- src/pages/RecipeForm.js
+++ src/pages/RecipeForm.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const RecipeForm = () => {
-  return <div>RecipeForm</div>;
+  return (
+    <div>
+      RecipeForm
+      <div></div>
+    </div>
+  );
 };
 
 export default RecipeForm;
